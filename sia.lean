be6f964a94e695db358a4fa98f5cbba3fe580288
Naman Bharadwaj <naman@kite.com>
2017-10-13 11:01:50 -0700

    microcancellation, and fixes

--- sia.lean
+++ sia.lean
@@ -2,25 +2,32 @@ prelude
 import init.algebra.field
 import init.algebra.order
 import init.logic
+import init.core
 universe u
+
+notation `exists!` binders `, ` r:(scoped P, exists_unique P) := r
 
 
 variable {R : Type u}
 variable [ring R]
 
-definition D (a : R): Prop := a * a = 0
-definition z_mul_z_eq_z : D (0 : R) := ring.mul_zero 0
-def derivative_test (d : R) (pf : D d) (f : (Pi r : R, D r -> R)) (a : R) : Prop := f d pf = f 0 z_mul_z_eq_z + a * d
+@[reducible]
+definition microneighborhood (around : R) : Type u := { r: R // r * r = around }
+
+@[reducible]
+def Delta : Type u := microneighborhood (0 : R)
+@[reducible]
+instance : has_zero Delta := (| { val := (0 : R), property := ring.mul_zero (0 : R) } |)
 
 -- Smooth Infinitesimal Analysis
 class sia R extends field R, strict_order R :=
 -- Axiom 1 (field R)
 -- Axiom 2 (strict_order R); we can't just use ordered_field due to antisymmetric ≤
 (zero_lt_one :            0 < 1)
-(add_lt_add_left :        forall a b : R, a < b → forall c : R, c + a < c + b)
-(mul_lt_mul_of_pos_left : forall a b c : R, a < b → 0 < c → c * a < c * b)
-(neq_distinguishable :    forall a b : R, a ≠ b → a < b ∨ b < a)
+(add_lt_add_left :        forall a b : R, a < b -> forall c : R, c + a < c + b)
+(mul_lt_mul_of_pos_left : forall a b c : R, a < b -> 0 < c -> c * a < c * b)
+(ne_distinguishable :     forall a b : R, ne a b -> a < b \/ b < a)
 -- Axiom 3
-(exists_unique_sqrt :     forall a : R, ∃! b, b * b = a)
+(exists_unique_sqrt :     forall a : R, exists! b, b * b = a)
 -- Axiom 4
-(kock_lawvere : forall f: (Pi r:R, D r -> R), forall d:R, Pi pf: D d, exists_unique (derivative_test d pf f))
+(kock_lawvere :           forall f: Delta -> R, forall d: Delta, exists! a: R, f d = f 0 + a * d.val)
