910201bac21d926d7bd67e22a41c70a6635c71c9
Naman Bharadwaj <naman@kite.com>
2017-10-18 11:57:58 -0700

    cleanup

--- src/exercises.lean
+++ src/exercises.lean
@@ -9,13 +9,13 @@ theorem neg_flips_order : forall {a b : R}, a < b -> -b < -a :=
     assume a b,
     assume lt: a < b,
     calc
-        -b  = -b + 0        : eq.symm (add_zero _)
-        ... = -b + (-a + a) : congr_arg _ (eq.symm (add_left_neg _))
-        ... = (-b + -a) + a : eq.symm (add_assoc _ _ _)
+        -b  = -b + 0        : by rw (add_zero _)
+        ... = -b + (-a + a) : by rw (add_left_neg _)
+        ... = (-b + -a) + a : by rw (add_assoc _ _ _)
         ... < (-b + -a) + b : sia.add_lt_add_left lt _
         ... = (-a + -b) + b : congr_arg (fun x, x + b) (add_comm _ _)
         ... = -a + (-b + b) : add_assoc _ _ _
-        ... = -a + 0        : congr_arg _ (add_left_neg _)
+        ... = -a + 0        : by rw (add_left_neg _)
         ... = -a            : add_zero _
 
 
@@ -25,9 +25,9 @@ section -- 1.1
     example : 0 < a -> ne 0 a :=
         assume a_pos,
         assume a_zero_bad: 0 = a,
-        strict_order.lt_irrefl 0 (calc
+        strict_order.lt_irrefl (0: R) (calc
             0   < a : a_pos
-            ... = 0 : eq.symm a_zero_bad
+            ... = 0 : by rw a_zero_bad
         )
 
     example : 0 < a <-> -a < 0 :=
@@ -40,7 +40,7 @@ section -- 1.1
         have backwards : -a < 0 -> 0 < a, from (
             assume neg_a_neg,
             calc
-                0   = -0    : eq.symm (neg_zero)
+                0   = -0    : by rw neg_zero
                 ... < -(-a) : neg_flips_order neg_a_neg
                 ... = a     : neg_neg _
         ),
@@ -56,7 +56,7 @@ section -- 1.1
          have left: a < 0 -> 0 < a * a, from (
             assume a_neg,
             have neg_a_pos: 0 < -a, from (calc
-                0   = -0 : eq.symm neg_zero
+                0   = -0 : by rw neg_zero
                 ... < -a : neg_flips_order a_neg
             ), calc
                 0   = -a * 0     : by rw (mul_zero _)
@@ -66,7 +66,7 @@ section -- 1.1
          have right: 0 < a -> 0 < a * a, from (
              assume a_pos,
              calc
-                 0   = a * 0 : eq.symm (ring.mul_zero a)
+                 0   = a * 0 : by rw (mul_zero a)
                  ... < a * a : sia.mul_lt_mul_of_pos_left a_pos a_pos
          ),
          or.elim either_lt_0_a left right
