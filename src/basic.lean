e9814c73388610d5ed718472d52b66e947dbc7d2
Naman Bharadwaj <naman@kite.com>
2017-10-17 17:46:55 -0700

    intervals, fix sqrt axiom, generalized microaffinity

--- src/basic.lean
+++ src/basic.lean
@@ -1,4 +1,5 @@
 import init.algebra.ring
+import init.algebra.group
 import .core
 
 variable (R : Type)
@@ -18,4 +19,15 @@ theorem microcancellation : forall a b: R, (forall d: Delta, a * d.val = b * d.v
             intro d,
             apply eq.trans (ea_eq_eb d),
             reflexivity,
+    end
+
+theorem microaffinity : forall f: R -> R, forall x: R, exists! a: R, forall d: Delta, f (x + d.val) = f x + a * d.val :=
+    assume f: R -> R,
+    assume x: R,
+    let g (d: Delta) : R := f (x + d.val) in
+    have nice: f x = g 0, from eq.symm (eq.subst (add_group.add_zero x) (eq.refl _)),
+    begin
+        show exists! a: R, forall d: Delta, g d = f x + a * d.val,
+        rewrite nice,
+        apply sia.kock_lawvere,
     end
--- src/core.lean
+++ src/core.lean
@@ -9,24 +9,34 @@ notation `exists!` binders `, ` r:(scoped P, exists_unique P) := r
 
 
 variable {R : Type u}
-variable [ring R]
 
-@[reducible]
-definition microneighborhood (around : R) : Type u := { r: R // r * r = around }
+section -- microneighborhoods
+    variable [ring R]
 
-@[reducible]
-def Delta : Type u := microneighborhood (0 : R)
-@[reducible]
-instance : has_zero Delta := (| { val := (0 : R), property := ring.mul_zero (0 : R) } |)
+    @[reducible]
+    definition microneighborhood (around : R) : Type u := { r: R // r * r = around }
+
+    @[reducible]
+    def Delta : Type u := microneighborhood (0 : R)
+
+    @[reducible]
+    instance : has_zero Delta := (| { val := (0 : R), property := ring.mul_zero (0 : R) } |)
+end
 
 -- Smooth Infinitesimal Analysis
 class sia R extends field R, strict_order R :=
--- Axiom 1 (field R)
--- Axiom 2 (strict_order R); we can't just use ordered_field due to antisymmetric ≤
-(zero_lt_one :            0 < 1)
-(add_lt_add_left :        forall a b : R, a < b -> forall c : R, c + a < c + b)
-(mul_lt_mul_of_pos_left : forall a b c : R, a < b -> 0 < c -> c * a < c * b)
--- Axiom 3
-(exists_unique_sqrt :     forall a : R, exists! b, b * b = a)
--- Axiom 4 : principle of microaffineness
-(kock_lawvere :           forall f: Delta -> R, exists! a: R, forall d: Delta, f d = f 0 + a * d.val)
+    (zero_lt_one :            0 < 1)
+    (add_lt_add_left :        forall a b : R, a < b -> forall c : R, c + a < c + b)
+    (mul_lt_mul_of_pos_left : forall a b c : R, a < b -> 0 < c -> c * a < c * b)
+    (exists_unique_sqrt :     forall a : { r: R // r > 0 }, exists! b, b * b = a.val)
+    (kock_lawvere :           forall f: Delta -> R, exists! a: R, forall d: Delta, f d = f 0 + a * d.val)
+
+section -- intervals
+    variable [sia R]
+
+    definition open_interval (a: R) (b: R) : Type u := { r: R // a < r /\ r < b }
+    definition closed_interval (a: R) (b: R) : Type u := { r: R // a <= r /\ r <= b }
+
+    notation `int[` a `,` b `]` := open_interval a b
+    notation `int(` a `,` b `(` := closed_interval a b
+end
