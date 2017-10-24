62fc7172783ec391db5bf593ec1282e85f2f4685
Naman Bharadwaj <naman@kite.com>
2017-10-24 13:57:03 -0700

    ≤ lemmas

--- src/core.lean
+++ src/core.lean
@@ -20,21 +20,19 @@ section -- microneighborhoods
     instance : has_zero Delta := (| { val := (0 : R), property := ring.mul_zero (0 : R) } |)
 end
 
--- strict order
-class strict_order (t : Type u) extends has_le t, has_lt t :=
-    (lt_irrefl : forall a : t, not (a < a))
-    (lt_trans : forall {a b c : t}, a < b -> b < c -> a < c)
-    (le_iff_not_rev_lt : forall {a b : t}, a <= b <-> not (b < a))
-    (ne_distinguishable : forall {a b : t}, ne a b -> a < b \/ b < a)
-attribute [trans] strict_order.lt_trans -- allow use of transitivity in calc proofs
-
 -- Smooth Infinitesimal Analysis
-class sia R extends field R, strict_order R :=
-    (zero_one_far :           forall a: R, 0 < a \/ a < 1)
-    (add_lt_add_left :        forall {a b : R}, a < b -> forall c : R, c + a < c + b)
-    (mul_lt_mul_of_pos_left : forall {a b c : R}, a < b -> 0 < c -> c * a < c * b)
-    (exists_unique_sqrt :     forall a : { r: R // r > 0 }, exists! b, b * b = a.val)
-    (kock_lawvere :           forall f: Delta -> R, exists! a: R, forall d: Delta, f d = f 0 + a * d.val)
+class sia R extends field R, has_lt R, has_le R :=
+    (lt_irrefl : forall a : R, not (a < a))
+    (lt_trans  : forall {a b c : R}, a < b -> b < c -> a < c)
+    (le_def    : forall {a b : R}, a <= b <-> not (b < a))
+    (ne_lt     : forall {a b : R}, ne a b -> a < b \/ b < a)
+
+    (zero_one_far       : forall a: R, 0 < a \/ a < 1)
+    (lt_add_left        : forall {a b : R}, a < b -> forall c : R, c + a < c + b)
+    (lt_mul_pos_left    : forall {a b c : R}, a < b -> 0 < c -> c * a < c * b)
+    (exists_unique_sqrt : forall a : { r: R // r > 0 }, exists! b, b * b = a.val)
+    (kock_lawvere       : forall f: Delta -> R, exists! a: R, forall d: Delta, f d = f 0 + a * d.val)
+attribute [trans] sia.lt_trans -- allow use of transitivity in calc proofs
 
 section -- intervals
     variable [sia R]
@@ -43,12 +41,13 @@ section -- intervals
     definition closed_interval (a: R) (b: R) : Type u := { r: R // a <= r /\ r <= b }
 
     notation `[` a `...` b `]` := open_interval a b
-    notation `[(` a `...` b `)]` := closed_interval a b
+    notation `[[` a `...` b `]]` := closed_interval a b
 end
 
 section
     variable [sia R]
 
-    @[reducible]
+    -- non-unicode notation for inverse
+    @[simp, reducible]
     def inv : R -> R := has_inv.inv
 end
--- src/exercises.lean
+++ src/exercises.lean
@@ -1,6 +1,5 @@
 import .core
 import .basic
-import init.algebra.group
 
 variable {R : Type}
 variable [sia R]
@@ -12,7 +11,7 @@ section -- 1.1
     example : 0 < a -> ne 0 a :=
         assume a_pos,
         assume a_zero_bad: 0 = a,
-        strict_order.lt_irrefl (0: R) (calc
+        sia.lt_irrefl (0: R) (calc
             0   < a : a_pos
             ... = 0 : by rw a_zero_bad
         )
@@ -21,22 +20,22 @@ section -- 1.1
         have forwards : 0 < a -> -a < 0, from (
             assume a_pos,
             calc
-                -a  < -0 : neg_flips_order a_pos
+                -a  < -0 : sia.lt_neg_flip a_pos
                 ... = 0  : neg_zero
         ),
         have backwards : -a < 0 -> 0 < a, from (
             assume neg_a_neg,
             calc
                 0   = -0    : by rw neg_zero
-                ... < -(-a) : neg_flips_order neg_a_neg
+                ... < -(-a) : sia.lt_neg_flip neg_a_neg
                 ... = a     : by rw neg_neg
         ),
         iff.intro forwards backwards
 
     example : 0 < (1: R) + 1 := calc
-        0   < 1           : sia.zero_lt_one
+        0   < 1           : sia.lt_zero_one
         ... = 1 + 0       : eq.symm (add_zero 1)
-        ... < (1 + 1 : R) : sia.add_lt_add_left sia.zero_lt_one 1
+        ... < (1 + 1 : R) : sia.lt_add_left sia.lt_zero_one 1
 
      example : a < 0 \/ 0 < a -> 0 < a * a :=
          assume either_lt_0_a,
@@ -44,55 +43,28 @@ section -- 1.1
             assume a_neg,
             have neg_a_pos: 0 < -a, from (calc
                 0   = -0 : by rw neg_zero
-                ... < -a : neg_flips_order a_neg
+                ... < -a : sia.lt_neg_flip a_neg
             ), calc
                 0   = -a * 0     : by rw mul_zero
-                ... < -a * -a    : sia.mul_lt_mul_of_pos_left neg_a_pos neg_a_pos
+                ... < -a * -a    : sia.lt_mul_pos_left neg_a_pos neg_a_pos
                 ... = a * a      : by rw neg_mul_neg
          ),
          have right: 0 < a -> 0 < a * a, from (
              assume a_pos,
              calc
                  0   = a * 0 : by rw (mul_zero a)
-                 ... < a * a : sia.mul_lt_mul_of_pos_left a_pos a_pos
+                 ... < a * a : sia.lt_mul_pos_left a_pos a_pos
          ),
          or.elim either_lt_0_a left right
 end
 
+-- 1.2 in basic.lean
 
-example : forall {a b: R}, a < b -> forall x: R, a < x \/ x < b := -- 1.2
+example : forall {a b: R}, not (a < b) -> [a ... b] -> false := -- 1.3; i.e. [a ... b] is empty
     assume a b,
-    assume a_lt_b,
-    assume x,
+    assume not_a_lt_b,
+    assume bad_elem,
+    have bad: a < bad_elem.val /\ bad_elem.val < b, from bad_elem.property,
+    not_a_lt_b (sia.lt_trans (and.elim_left bad) (and.elim_right bad))
 
-    -- shift x by [a ... b] -> [0 ... 1]
-    let delta: R := b - a in
-    let y: R := (x - a) / delta in
-
-    have delta_pos : 0 < b - a, from (calc
-        0   = -a + a : by rw add_left_neg
-        ... < -a + b : sia.add_lt_add_left a_lt_b _
-        ... =  b - a : by rw [add_comm, sub_eq_add_neg]
-    ),
-    have delta_ne_zero : ne (b - a) 0, from ne.symm (lt_ne delta_pos),
-
-    have almost: 0 < y \/ y < 1, from sia.zero_one_far y,
-    have left: 0 < y -> a < x \/ x < b, from
-        assume y_pos : 0 < y,
-        or.intro_left _ (calc -- show a < x
-            a   = a + delta * 0               : by rw [mul_zero, add_zero a]
-            ... < a + delta * y               : sia.add_lt_add_left (sia.mul_lt_mul_of_pos_left y_pos delta_pos) _
-            ... = a + (x - a) / delta * delta : by rw mul_comm
-            ... = a + (x - a)                 : by rw div_mul_cancel _ delta_ne_zero
-            ... = x                           : by rw [add_comm, sub_add_cancel]
-        ),
-    have right: y < 1 -> a < x \/ x < b, from
-        assume y_lt_one: y < 1,
-        or.intro_right _ (calc -- show x < b
-            x   = (x - a) + a   : by rw sub_add_cancel
-            ... = a + delta * y : by rw [mul_comm, add_comm, div_mul_cancel _ delta_ne_zero]
-            ... < a + delta * 1 : sia.add_lt_add_left (sia.mul_lt_mul_of_pos_left y_lt_one delta_pos) _
-            ... = a + b - a     : by rw [mul_one, add_sub_assoc]
-            ... = b             : by rw [sub_eq_add_neg, add_comm, neg_add_cancel_left]
-        ),
-    or.elim almost left right
+-- 1.4 in basic.lean
