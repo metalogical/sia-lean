31c4b3f4f70bca78546051220579387b7142b561
Naman Bharadwaj <naman@kite.com>
2017-12-04 14:35:09 -0800

    minor cleanup

--- src/sia.lean
+++ src/sia.lean
@@ -7,27 +7,25 @@ variable {R : Type u}
 section -- define Delta in terms of ring to avoid circularity in the definition of sia
     variable [ring R]
 
-    @[reducible]
-    private def Delta : set R := fun r, r * r = 0
-
-    @[reducible]
-    private def zero_Delta : has_zero (subtype Delta) := { zero := { val := (0 : R), property := ring.mul_zero (0 : R) } }
+    @[reducible] private def DeltaT := { x:R // x * x = 0 }
+    @[reducible] private def zDeltaT : DeltaT := { val := (0 : R), property := ring.mul_zero (0 : R) }
 end
 
 -- Smooth Infinitesimal Analysis
 class sia R extends st_ordered_field R :=
     (exists_unique_sqrt : forall a : { r: R // r > 0 }, exists! b, b * b = a.val) -- we have at least the real numbers (? is this equivalent to LUB)
-    (kock_lawvere       : forall f: subtype Delta -> R, exists! a: R, forall d: subtype Delta, f d = f zero_Delta.zero + a * d.val)
+    (kock_lawvere       : forall f: DeltaT -> R, exists! a: R, forall d: DeltaT, f d = f zDeltaT + a * d.val)
 
 namespace sia -- intervals
     variable [sia R]
 
     @[reducible]
-    def microstable (A : set R) : Prop := forall a : subtype A, forall d : subtype Delta, set.mem (a.val + d.val) A
+    def microstable (A : set R) : Prop := forall a : subtype A, forall d : DeltaT, set.mem (a.val + d.val) A
 end sia
 
 namespace sia
     -- export Delta in terms of sia; type parameter R is explicit because Lean cannot typically infer it
     @[reducible] def Delta (R: Type u) [sia R] := fun r : R, r * r = 0
-    instance {R : Type u} [sia R] : has_zero (subtype (Delta R)) := (| { val := (0 : R), property := ring.mul_zero (0 : R) } |)
+    @[reducible] def DeltaT (R: Type u) [sia R] := subtype (Delta R)
+    instance {R : Type u} [sia R] : has_zero (DeltaT R) := (| zDeltaT |)
 end sia
